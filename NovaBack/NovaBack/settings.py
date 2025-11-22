421f656c942f0477b0b202e0316b010bab4d35c3
Camilo-Ota <camilootri@unisabana.edu.co>
2025-11-22 15:51:01 -0500

    Fallos test usuario

--- NovaBack/NovaBack/settings.py
+++ NovaBack/NovaBack/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'metas'
+    'metas',
+    'usuarios',
 ]
 
 MIDDLEWARE = [
--- NovaBack/usuarios/tests.py
+++ NovaBack/usuarios/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Usuario
 
 # Create your tests here.
+
+class UsuarioModelTest(TestCase):
+    def test_crear_usuario(self):
+        Usuario.objects.create(username='testuser', password='testpass')
+        self.assertTrue(Usuario.objects.filter(username='testuser').exists())
+        
+    def balance_test(self):
+        user = Usuario.objects.create(username='balanceuser', password='balancepass', balance=100)
+        self.assertEqual(user.balance, 100)
+        
+    def positive_balance_test(self):
+        user = Usuario.objects.create(username='positiveuser', password='positivepass', balance=50)
+        self.assertGreaterEqual(user.balance, 0)
